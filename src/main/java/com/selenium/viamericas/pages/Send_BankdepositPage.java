9a6647b8f8eef351d90b0a1f9085cb2a05b4e0b3
Esneider Gaviria <esneider.gaviria@objectwave.com>
2017-03-27 18:21:25 -0500

    Added account type to language

--- src/main/java/com/selenium/viamericas/pages/Send_BankdepositPage.java
+++ src/main/java/com/selenium/viamericas/pages/Send_BankdepositPage.java
@@ -1,5 +1,6 @@
 package com.selenium.viamericas.pages;
 
+import com.selenium.viamericas.utility.Language;
 import com.selenium.viamericas.utility.Start;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -7,9 +8,11 @@ import org.openqa.selenium.Keys;
 
 public class Send_BankdepositPage {
 
+    public static Language language = new Language();
+
     public static By accountholdername = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[2]/input");
     public static By accountnumber = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[3]/input");
-    public static By accounttypedropdown = By.xpath(".//*[@placeholder='Account Type']");
+    public static By accounttypedropdown = By.xpath(".//*[@placeholder='"+ language.getaccountType()+"']");
     public static By continueButton = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[2]/button[2]");
     public static By backButton = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[2]/button[1]");
 
