d8fcc2f2fdecf685c0b28e708575a58e771f8f2b
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-03-28 14:49:13 -0500

    Updating home page for Viamodal

--- src/main/java/com/selenium/viamericas/pages/viaModal/HomePage.java
+++ src/main/java/com/selenium/viamericas/pages/viaModal/HomePage.java
@@ -1,5 +1,6 @@
 package com.selenium.viamericas.pages.viaModal;
 
+import com.selenium.viamericas.utility.Start;
 import com.selenium.viamericas.utility.StartViamodal;
 import org.openqa.selenium.By;
 
@@ -13,29 +14,31 @@ public class HomePage {
     public static By SignupButton = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div[2]/div[2]/button[2]");
     public static By GetaQuoteLabel = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div[2]/div[3]/button[1]/span");
     public static By FindaLocationLabel = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div[2]/div[3]/button[2]/span");
+    public static By intercom = By.xpath("//*[@id=\"intercom-container\"]/div/div[2]");
 
-    public static void GotoSendMoney() throws Exception{
-        StartViamodal.driver.findElement(SendMoneyButton).click();
-        Thread.sleep(2000);
-    }
 
-    public static void GotoLogin() throws Exception{
-        StartViamodal.driver.findElement(LoginButton).click();
-        Thread.sleep(2000);
-    }
+    public static void Goto(String page) throws Exception {
 
-    public static void GotoSingup() throws Exception{
-        StartViamodal.driver.findElement(SignupButton).click();
-        Thread.sleep(2000);
-    }
+        if(page.equals("sendmoney")){
+            Start.driver.findElement(SendMoneyButton).click();
 
-    public static void GetaQuoteLabel() throws Exception{
-        StartViamodal.driver.findElement(GetaQuoteLabel).click();
-        Thread.sleep(2000);
-    }
+        }else if(page.equals("login")){
+            Start.driver.findElement(LoginButton).click();
 
-    public static void FindaLocationLabel() throws Exception{
-        StartViamodal.driver.findElement(FindaLocationLabel).click();
-        Thread.sleep(2000);
-    }
+        }else if(page.equals("signup")) {
+            Start.driver.findElement(SignupButton ).click();
+            Thread.sleep(3000);
+
+        }else if(page.equals("getaquote")) {
+            Start.driver.findElement(GetaQuoteLabel).click();
+
+        }else if(page.equals("findalocation")){
+            Start.driver.findElement(FindaLocationLabel).click();
+
+        }else if(page.equals("intercom")){
+            Start.driver.findElement(intercom).click();
+            Thread.sleep(2000);
+        }
+}
+
 }
