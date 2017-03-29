204332314f76d28f0aa763b1a0c3ddd64317d220
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-03-29 16:14:15 -0500

    Some comments for the flow of the tests

--- src/main/java/com/selenium/viamericas/tests/Coupons.java
+++ src/main/java/com/selenium/viamericas/tests/Coupons.java
@@ -23,6 +23,38 @@ public class Coupons {
     7. What happen if I have two different Coupons? By default this select the first option
      */
 
+    /*
+    Flow:
+    1. As a guest
+        -Go to destination information (Test with cash pickup and bank deposit)
+        -Go to recipient information
+        -Go to Recipient bank Account
+        -Go to Funding Information (test with bank account and cards: credit and debit)
+        -Go to Sign Up flow (Add identifier)
+        -Go to Review Page (Review discount)
+        -Complete the transaction
+
+    2. As a user already registered and already created an identifier
+        -Login
+        -Go to destination information (Test with cash pickup and bank deposit)
+        -Go to Recipient information
+        -Go to Recipient bank Account
+        -Go to Funding Information (test with bank account and cards: credit and debit)
+        -Go to review page (review discount based in the identifier)
+        -Complete the transaction
+
+     3. As a user already registered but without create the identifier
+        -Login
+        -Go to profile settings
+        -Create a new identifier
+        -Go to destination screen (Test with cash pickup and bank deposit)
+        -Go to Recipient information
+        -Go to Recipient bank Account
+        -Go to Funding Information (test with bank account and cards: credit and debit)
+        -Go to review page (review discount based in the identifier)
+        -Complete the transaction
+     */
+
     @BeforeClass
     public void start() throws Exception {
         Start.initiate("test");
