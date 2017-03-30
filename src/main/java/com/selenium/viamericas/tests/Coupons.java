bb3db6407cb78357ffa9b7a7540c66b8cb8d1ac0
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-03-30 15:59:43 -0500

    Updating the coupon tests

--- src/main/java/com/selenium/viamericas/tests/Coupons.java
+++ src/main/java/com/selenium/viamericas/tests/Coupons.java
@@ -1,4 +1,6 @@
 package com.selenium.viamericas.tests;
+import com.selenium.viamericas.pages.*;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -62,8 +64,93 @@ public class Coupons {
 
     @AfterClass
     public void finish() {
-        Start.driver.quit();
+        //Start.driver.quit();
+    }
+
+    @Test (enabled = true, priority = 0)
+    public void FillSendMoneyAsaGuest() throws Exception {
+        HomePage.Goto("sendmoney");
+        Send_DestinationPage.selectCountry();
+        Send_DestinationPage.howMoneyRecieved("BankDeposit");
+        Send_DestinationPage.chooseBank();
+        Assert.assertNotNull(Send_DestinationPage.exchangerate);
+        Send_DestinationPage.selectamounttoSend();
+        Send_DestinationPage.goandcontinue();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("recipient"));
+        Send_RecipientPage.completename();
+        Send_RecipientPage.completelastname();
+        Send_RecipientPage.completephonenumber();
+        Send_RecipientPage.addadressline1();
+        Send_RecipientPage.emailaddress();
+        Send_RecipientPage.selectmonth();
+        Send_RecipientPage.selectday();
+        Send_RecipientPage.selectyear();
+        Send_RecipientPage.selectstate();
+        Send_RecipientPage.selectcity();
+        Send_RecipientPage.continuebutton();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("bankdeposit"));
+        Send_BankdepositPage.Completeaccountname();
+        Send_BankdepositPage.Completeaccountnumber();
+        Send_BankdepositPage.selectaccounttype();
+        //Send_BankdepositPage.goandcontinue();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("fast-send/funding"));
+        Send_FundingPage.selectaccount("bankaccount");
+        Send_FundingPage.continuebutton();
+        Send_FundingPage.addbankholdername();
+        Send_FundingPage.addbanknickname();
+        Send_FundingPage.addroutingnumber();
+        Send_FundingPage.addnumber();
+        Send_FundingPage.selecttype();
+        //Send_FundingPage.continuebutton();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("fast-send-money/account/pre-register"));
+
+}
+
+    @Test (enabled = true, priority = 1)
+    public void FillSendMoneywithLogin() throws Exception {
+        HomePage.Goto("login");
+        LoginPage.typeemail("laura.alvarez@talosdigital.com");
+        LoginPage.typepassword("Laura123");
+        LoginPage.clicklogin();
+        HomePage.Goto("sendmoney");
+        Send_DestinationPage.selectCountry();
+        Send_DestinationPage.howMoneyRecieved("BankDeposit");
+        Send_DestinationPage.chooseBank();
+        Assert.assertNotNull(Send_DestinationPage.exchangerate);
+        Send_DestinationPage.selectamounttoSend();
+        Send_DestinationPage.goandcontinue();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("recipient"));
+        Send_RecipientPage.completename();
+        Send_RecipientPage.completelastname();
+        Send_RecipientPage.completephonenumber();
+        Send_RecipientPage.addadressline1();
+        Send_RecipientPage.emailaddress();
+        Send_RecipientPage.selectmonth();
+        Send_RecipientPage.selectday();
+        Send_RecipientPage.selectyear();
+        Send_RecipientPage.selectstate();
+        Send_RecipientPage.selectcity();
+        Send_RecipientPage.continuebutton();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("bankdeposit"));
+        Send_BankdepositPage.Completeaccountname();
+        Send_BankdepositPage.Completeaccountnumber();
+        Send_BankdepositPage.selectaccounttype();
+        //Send_BankdepositPage.goandcontinue();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("funding"));
+        Send_FundingPage.selectaccount("bankaccount");
+        Send_FundingPage.continuebutton();
+        Send_FundingPage.addbankholdername();
+        Send_FundingPage.addbanknickname();
+        Send_FundingPage.addroutingnumber();
+        Send_FundingPage.addnumber();
+        Send_FundingPage.selecttype();
+        Send_FundingPage.continuebutton();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("review"));
+        Send_ReviewPage.sendmoneybutton();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("confirmation"));
+
     }
 
 
 }
+
