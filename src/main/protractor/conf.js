36284b94623ecef94240c7c2d338db1712cec49b
Esneider Gaviria <esneider.gaviria@objectwave.com>
2017-06-05 11:08:38 -0500

    Fixing the config.js and Specs.js Protractor

--- src/main/protractor/conf.js
+++ src/main/protractor/conf.js
@@ -1,13 +1,26 @@
 /**
  * Created by lauraalvarez on 2/06/17.
  */
+
+// An example configuration file.
 exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js', ''],
-    multiCapabilities: [{
-        browserName: 'firefox'
-    }, {
-        browserName: 'chrome'
-    }]
-}
+  directConnect: true,
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
