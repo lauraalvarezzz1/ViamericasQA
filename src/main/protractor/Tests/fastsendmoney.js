44456e6581be26bb5e73ee0eaea71ecf4464582d
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-06-16 12:15:37 -0500

    Solution with the dropdowns (FINALLY)

---
+++ src/main/protractor/Tests/fastsendmoney.js
@@ -0,0 +1,62 @@
+/**
+ * Created by lauraalvarez on 5/06/17.
+ */
+
+
+describe('Fast send money Testing - Viamericas Web App', function() {
+    var Country = browser.element(by.css("dropdown-viamericas#country-select"));
+    var Currency= browser.element(by.css("dropdown-viamericas#dropdown-input"));
+
+    beforeEach(function() {
+
+        browser.get('https://dev.govianex.com/#/fast-send/destination');
+        browser.wait(function() {
+            browser.executeScript('window.scrollTo(0,200);').then(function () {
+            })
+        }, 3000);
+
+    });
+
+    it('should start out with an empty memory', function () {
+        browser.sleep(2000);
+
+        var plot0 = element(by.css('body'));
+        browser.actions()
+            .mouseMove(plot0, {x: 100, y: 100})
+            .mouseDown()
+            .mouseMove({x: 400, y: 0}) // 400px to the right of current location
+            .perform();
+        browser.sleep(2000);
+
+        element(by.css('.intercom-launcher-frame')).click();
+
+        element(by.css('.intercom-launcher-frame')).click();
+        var selectcountry =
+            element.all(by.css('dropdown-viamericas#country-select ul.dropdown-viam-list li'))
+                .count().then(function(count) {
+                var ran = Math.floor((Math.random() * count) + 1);
+                Country.element(by.css('input#country-select')).click();
+                Country.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
+                console.log(ran);
+                browser.pause();
+
+            });
+
+        browser.sleep(2000);
+
+        var Currency =
+            element(by.id('dropdown-input'))
+                .isDisplayed().then(function (isVisible) {
+            if (isVisible) {
+                console.log("lo muestra");
+            } else {
+                console.log("no lo muestra");
+            }
+        });
+
+    });
+
+
+
+});
+
--- src/main/protractor/conf.js
+++ src/main/protractor/conf.js
@@ -12,7 +12,12 @@ exports.config = {
 
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
-      'browserName': 'chrome'
+      'browserName': 'chrome',
+        chromeOptions: {
+            args: [
+                '--start-maximized'
+            ]
+        }
   },
 
     // Framework to use. Jasmine is recommended.
@@ -20,7 +25,7 @@ exports.config = {
 
     // Spec patterns are relative to the current working directory when
     // protractor is called.
-    specs: ['recipients.js'],
+    specs: ['Tests/fastsendmoney.js'],
 
     // Options to be passed to Jasmine.
     jasmineNodeOpts: {
@@ -32,16 +37,6 @@ exports.config = {
         browser.manage().timeouts().setScriptTimeout(60000);
         browser.manage().timeouts().pageLoadTimeout(40000);
         browser.manage().timeouts().implicitlyWait(25000);
-
-        browser._selectDropdownbyNum = function (element, optionNum) {
-            /* A helper function to select in a dropdown control an option
-             * with specified number.
-             */
-            return element.all(by.tagName('option')).then(
-                function(options) {
-                    options[optionNum].click();
-                });
-        };
     },
 };
 
