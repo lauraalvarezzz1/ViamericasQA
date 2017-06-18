cd55402253e76b6103b7802b0bbf583b1d05bcbe
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-06-18 04:13:48 -0500

    Complete transaction information tests with any flow (Cash pickup with national and regional networks and banks deposit)

--- src/main/protractor/Tests/fastsendmoney.js
+++ src/main/protractor/Tests/fastsendmoney.js
@@ -31,14 +31,11 @@ describe('Fast send money Testing - Viamericas Web App', function() {
                 var ran = Math.floor((Math.random() * countCountries) + 1);
                 sendmoneyFlowPage.country.element(by.css('input#country-select')).click();
                 sendmoneyFlowPage.country.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
-                console.log(ran);
-
             });
 
-        browser.sleep(5000);
+        browser.sleep(2000);
 
         sendmoneyFlowPage.currency.isDisplayed().then(function(rs){
-            console.log(rs);
             if(rs){
                 console.log("This have more than 1 currency available");
                 var selectcurrency =
@@ -47,16 +44,14 @@ describe('Fast send money Testing - Viamericas Web App', function() {
                         var ran = Math.floor((Math.random() * countCurrencies) + 1);
                         sendmoneyFlowPage.currency.element(by.css('input#recipient-currency-select')).click();
                         sendmoneyFlowPage.currency.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
-                        console.log(ran);
                     });
             }else{
                 console.log("The actual country only has one currency available");
             }
-            browser.sleep(5000);
+            browser.sleep(2000);
         });
 
         sendmoneyFlowPage.secondButton.isPresent().then(function(rs){
-            console.log(rs);
             if(rs) {
                 var button = Math.floor((Math.random() * 2) + 1);
                 if(button==1){
@@ -65,15 +60,68 @@ describe('Fast send money Testing - Viamericas Web App', function() {
                     sendmoneyFlowPage.secondButton.click();
                 }
             }else{
-                console.log("solo hay uno");
                 sendmoneyFlowPage.firstButton.click();
             }
+
+           /* if (sendmoneyFlowPage.firstButton==element(by.partialButtonText('Bank Deposit'))||
+                sendmoneyFlowPage.firstButton== element(by.partialButtonText('Banco'))){
+                console.log("si es");
+            }else{
+                console.log("no es");
+            }*/
+            browser.sleep(2000);
+
+            sendmoneyFlowPage.state.isPresent().then(function(rs){
+                if(rs) {
+                    var selectstate =
+                        sendmoneyFlowPage.stateLI.count().then(function(countstates) {
+                            var ran = Math.floor((Math.random() * countstates) + 1);
+                            sendmoneyFlowPage.state.element(by.css('input#states-select')).click();
+                            sendmoneyFlowPage.state.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
+                            console.log("States available"+ ran);
+                        });
+
+                    browser.sleep(2000);
+
+                    var selectcity =
+                        sendmoneyFlowPage.cityLI.count().then(function(countcities) {
+                            var ran = Math.floor((Math.random() * countcities) + 1);
+                            sendmoneyFlowPage.city.element(by.css('input#cities-select')).click();
+                            sendmoneyFlowPage.city.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
+                            console.log("Cities available" +ran);
+
+                        });
+
+                    browser.sleep(2000);
+
+                    var selectregionalnetwork =
+                        sendmoneyFlowPage.regionalLI.count().then(function(countregional) {
+                            var ran = Math.floor((Math.random() * countregional) + 1);
+                            sendmoneyFlowPage.regional.element(by.css('input#regional-networks-select')).click();
+                            sendmoneyFlowPage.regional.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
+                            console.log("Regional Networks available" +ran);
+                        });
+                }else{
+                    sendmoneyFlowPage.firstbankintopfive.isPresent().then(function(rs){
+                        if(rs) {
+                            sendmoneyFlowPage.firstbankintopfive.click();
+                        }else{
+                            console.log("Select it automatically");
+                        }
+                    });
+                }
+            });
+            browser.sleep(2000);
+            sendmoneyFlowPage.amount.sendKeys(numbergenerator(1, 3000));
+            sendmoneyFlowPage.continueButton.click();
         });
 
         browser.pause();
-    });
 
-
+    }, 120000);
 
 });
 
+numbergenerator = function(min, max){
+    return parseInt(Math.random() * (max - min) + min);
+};
--- src/main/protractor/po/sendMoneyFlowPage.js
+++ src/main/protractor/po/sendMoneyFlowPage.js
@@ -23,6 +23,22 @@ var sendMoneyFlowPage = function() {
     this.secondButton = element(by.xpath('/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[3]/div[1]/div[2]/div'));
     this.amount = element(by.id('amount'));
 
+    //Choose a different bank - Dropdown
+    this.banksLI = element.all(by.css('dropdown-viamericas#undefined ul.dropdown-viam-list li'));
+    this.banks = element(by.css('dropdown-viamericas#undefined'));
+
+    //First Bank in the list
+    this.firstbankintopfive = element(by.xpath('/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[4]/div/ul/li[1]/label'));
+
+    this.stateLI = element.all(by.css('dropdown-viamericas#states-select ul.dropdown-viam-list li'));
+    this.state = element(by.css('dropdown-viamericas#states-select'));
+    this.cityLI = element.all(by.css('dropdown-viamericas#cities-select ul.dropdown-viam-list li'));
+    this.city = element(by.css('dropdown-viamericas#cities-select'));
+    this.regionalLI = element.all(by.css('dropdown-viamericas#regional-networks-select ul.dropdown-viam-list li'));
+    this.regional = element(by.css('dropdown-viamericas#regional-networks-select'));
+
+    this.amount = element(by.id('amount'));
+    this.continueButton = element(by.id('continue'));
 };
 
 module.exports = new sendMoneyFlowPage();
