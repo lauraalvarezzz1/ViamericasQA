2dd388b9e02cb4f2311c59c6077b1fe8d539f0db
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-06-19 04:03:26 -0500

    Sign up test only completing the sign up form

--- src/main/protractor/Tests/signup.js
+++ src/main/protractor/Tests/signup.js
@@ -2,45 +2,109 @@
 
 describe('Sign up testing Web App', function() {
 
-    var Signup = element(by.partialButtonText('Sign Up For Free'));
-    var Username = element(by.model('preRegister.newUserDTO.email'));
-    var password = element(by.model('preRegister.newUserDTO.password'));
-    var SignUpButton = element(by.partialButtonText('Sign Up'));
-    var ijustwanttosignupButton = element(by.partialButtonText('I just want to sign up'));
-
-    var firstname = element(by.model('register.formData.firstName'));
-    var lastname = element(by.model('register.formData.lastName'));
-    var mobilephonenumber = element(by.model('register.formData.phone1'));
-    var addressline1 = element(by.model('register.formData.address1'));
-    var zipcode = element(by.model('register.formData.zipCode'));
-    var selectDropdownElement = element(by.model('dropdown.selectedText'));
-
-
-
     beforeEach(function() {
         browser.get('https://dev.govianex.com/#/');
+        signUpPage = require('../po/signupPage');
+        homePage = require('../po/homePage');
+
     });
 
     it('Sign Up form', function() {
-        Signup.click();
-        Username.sendKeys("viamericas.testing+1@gmail.com");
-        password.sendKeys("Viamericas123");
-        SignUpButton.click();
 
-        ijustwanttosignupButton.click();
-        firstname.sendKeys("Viamericas");
-        lastname.sendKeys("Testing");
-        mobilephonenumber.sendKeys("3122342323");
-        addressline1.sendKeys("Testing");
-        zipcode.sendKeys("33233");
+        homePage.signUpButtonXpath.click();
+        signUpPage.email.sendKeys("viamericas.testing+"+numbergenerator(0000000000,9999999999)+"@gmail.com");
+        signUpPage.password.sendKeys("wHNLK096YHM");
 
-        selectDropdownElement.all(by.tagName('12'))
-            .then(function (options) {
-                options[0].click();
-            });
+        //Go to LOGIN FORM or SIGN UP FLOW
+        var gotoanyflow = Math.floor((Math.random() * 2) + 1);
+        if(gotoanyflow==1){
+            signUpPage.loginbutton.click();
+            expect(browser.getCurrentUrl()).toEqual('https://dev.govianex.com/#/account/login');
+        }else{
+            signUpPage.signupButton.click();
+            var signuporsendmoney = Math.floor((Math.random() * 2) + 1);
+            if(signuporsendmoney==1) {
+                signUpPage.justwantsignup.click();
 
-        browser.pause();
-    });
+                //Complete sign up form
+                signUpPage.first_name.sendKeys("Testing");
+                signUpPage.middle_name_optional.sendKeys("Testing");
+                signUpPage.last_name.sendKeys("Testing");
+                signUpPage.secondlast_name_optional.sendKeys("Testing");
+                signUpPage.mobile_phone_optional.sendKeys(numbergenerator(312000000, 312999999));
+                signUpPage.address_line1.sendKeys("Street 5 - Testing Address Line1");
 
+                signUpPage.month.sendKeys(numbergenerator(0,1));
+                if (signUpPage.month >= 1){
+                    signUpPage.month.sendKeys(numbergenerator(0,2));
+                } else{
+                    signUpPage.month.sendKeys(numbergenerator(1,9));
+                }
+                browser.sleep(6000);
+
+                signUpPage.year.sendKeys(numbergenerator(1900,1998));
+                browser.sleep(6000);
+
+                if (signUpPage.month >= 02){
+                    signUpPage.day.sendKeys(numbergenerator(0,2));
+                    if (signUpPage.day >= 0 || Day >= 1){
+                        signUpPage.day.sendKeys(numbergenerator(1,9));
+                    }else{
+                        signUpPage.day.sendKeys(numbergenerator(0,8));
+                    }
+                } else{
+                    signUpPage.day.sendKeys(numbergenerator(0,3));
+                    if (signUpPage.month==04 || signUpPage.month==06 || signUpPage.month==09 || signUpPage.month==11 && signUpPage.day==3){
+                        signUpPage.day.sendKeys(numbergenerator(0,0));
+                    }else{
+                        signUpPage.day.sendKeys(numbergenerator(1,9));
+                    }
+                }
+                browser.sleep(3000);
+
+                signUpPage.zipcode.sendKeys("90001"); //Pending for send aleatory zip codes
+                var selectcity =
+                    signUpPage.cityLI.count().then(function(countcities) {
+                        var ran = Math.floor((Math.random() * countcities) + 1);
+                        signUpPage.city.element(by.css('input#city-select')).click();
+                        signUpPage.city.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
+                        console.log("Cities available" +ran);
+
+                    });
+
+                //Register the user with/without program
+                var programornot = Math.floor((Math.random() * 2) + 1);
+                if(programornot==1) {
+                    var selectprogram =
+                        signUpPage.programLI.count().then(function(countprograms) {
+                            var ran = Math.floor((Math.random() * countprograms) + 1);
+                            signUpPage.program.element(by.css('input#partner-id-select')).click();
+                            signUpPage.program.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
+                            console.log("Programs available" +ran);
+
+                        });
+                    browser.sleep(2000);
+
+                    var selectunion =
+                        signUpPage.unionNameLI.count().then(function(countunions) {
+                            var ran = Math.floor((Math.random() * countunions) + 1);
+                            signUpPage.unionName.element(by.css('input#affiliate-name-select')).click();
+                            signUpPage.unionName.element(by.css('ul.dropdown-viam-list li:nth-child('+ran+')')).click();
+                            console.log("Unions available" +ran);
+
+                        });}
+                signUpPage.createAccountButton.click();
+                expect(browser.getCurrentUrl()).toEqual('https://dev.govianex.com/#/sendmoney/destination');
+
+            }else{
+                console.log("Go to fast send money flow");
+            }
+
+            browser.pause();
+        }});
 });
+
+numbergenerator = function(min, max){
+    return parseInt(Math.random() * (max - min) + min);
+};
 
