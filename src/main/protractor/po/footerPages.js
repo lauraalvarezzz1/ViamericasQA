6629a5fd1e28bb6ff37ee6b1abaaa7f4f2859f93
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-12-05 10:54:29 -0500

    Updating footer pages locators

--- src/main/protractor/po/footerPages.js
+++ src/main/protractor/po/footerPages.js
@@ -7,6 +7,7 @@
 
 var contactUs = function() {
     //Contact us form
+    this.contactuslink = element (by.id('contact-us-footer'));
     this.name = element(by.name('name'));
     this.email = element(by.id('email'));
     this.comments = element(by.id('comments'));
@@ -16,24 +17,32 @@ var contactUs = function() {
 
 var staticPages = function() {
     //About us form
+    this.aboutuslink = element(by.xpath('about-us-footer'));
     this.unoRanked = element(by.xpath('//*[@id="yui_3_16_0_ym19_1_1485211113917_117706"]/p[1]/a'));
     this.InternationalFinanceCorporation = element(by.xpath('//*[@id="yui_3_16_0_ym19_1_1485211113917_117706"]/p[2]/a[1]'));
     this.april = element(by.xpath('//*[@id="yui_3_16_0_ym19_1_1485211113917_117706"]/p[2]/a[2]'));
 
     //Compliance
+    this.compliancelink = element(by.id('compliance-footer'));
     this.stateCom = element.all(by.css('dropdown-viamericas#state ul.dropdown-viam-list li'));
     this.stateCom = element(by.css('dropdown-viamericas#state'));
 
     //File Complaint
+    this.fileacomplaintlink = element(by.id('file-a-complaint-footer'));
     this.alaskaCustomers = element(by.xpath('/html/body/div[2]/div/div[1]/div[2]/div/div/div/div/ul[2]/li[1]/a'));
     this.texasCustomers = element(by.xpath('/html/body/div[2]/div/div[1]/div[2]/div/div/div/div/ul[9]/li[2]/a'));
-    this.washingtonCustomers = element(by.xpath('/html/body/div[2]/div/div[1]/div[2]/div/div/div/div/ul[12]/li[2]/a'));
+    this.washingtonCustomersbyemail = element(by.xpath('/html/body/div[3]/div/div[1]/div[2]/div/div/div/div/ul[12]/li[1]/a'));
+    this.washingtonCustomersonline = element(by.xpath('/html/body/div[3]/div/div[1]/div[2]/div/div/div/div/ul[12]/li[2]/a'));
 
-    //Payer Network
-    this.countriesP = element.all(by.css('dropdown-viamericas#countries ul.dropdown-viam-list li'));
-    this.countriesP = element(by.css('dropdown-viamericas#countries'));
-
+    //Leadership - Careers - News - FAQ - TC - PP
+    this.leadershiplink = element(by.id('leadership-footer'));
+    this.leadershiplink = element(by.id('careers-footer'));
+    this.newslink = element(by.id('news-footer'));
+    this.faqslink = element(by.id('faq-footer'));
+    this.pplink = element(by.id('privacy-policy-footer'));
+    this.tclink = element(by.id('terms-and-conditions-footer'));
 };
 
 module.exports = new contactUs();
 module.exports = new staticPages();
+
