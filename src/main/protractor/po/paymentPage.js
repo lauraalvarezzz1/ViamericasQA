072304870f75aa1609dbbb0bc6e5f442bab63997
Laura Alvarez <laura.alvarez@talosdigital.com>
2018-01-19 10:28:59 -0500

    Adding ID’s for payment page, Login page, and fees and rates page

--- src/main/protractor/po/paymentPage.js
+++ src/main/protractor/po/paymentPage.js
@@ -7,20 +7,10 @@
 var paymentPage = function() {
 
     //payment method
-    this.paymentButton = element(by.partialButtonText('Payments'));
-    this.addpaymentButton = element(by.partialButtonText('Add a Payment'));
-    this.bankAccountButton = element(by.id('bank-account-button-settings'));
-    this.creditDebitButton = element(by.id('credit-debit-button-settings'));
-
-    //funding with bank accounts
-    this.bankAccountName = element(by.id("holder-name"));
-
-    //Plaid
-    this.addaccountplaid =element(by.id("plaid-link"));
-    this.bankwithplaid = element (by.css('#plaid-link-container .Overlay .App__content .Pane.InstitutionSelectPane .Pane__content li[data-institution="wells"]'));
-
-    this.usernameplaid = element (by.id("username"));
-    this.passwordplaid = element (by.id("password"));
+    this.paymentButton = element(by.id('payments-account'));
+    this.addpaymentButton = element(by.id('add-new-payment-payments'));
+    this.bankAccountButton = element(by.id('bank-account-button-settings-payment-details'));
+    this.creditDebitButton = element(by.id('credit-debit-button-settings-payment-details'));
 
     //Bank Account information
     this.accountNickname = element(by.id("account-nickname"));
@@ -30,17 +20,23 @@ var paymentPage = function() {
     this.accounttype = element(by.css('dropdown-viamericas#account-type-select'));
 
     //funding with credit or debit cards
-    this.cardtypeLI = element.all(by.css('dropdown-viamericas#card-type ul.dropdown-viam-list li'));
-    this.cardtypetype = element(by.css('dropdown-viamericas#card-type'));
-    this.creditCardName = element(by.id("cardholder-name"));
-    this.cardNickName = element(by.id("card-nickname"));
-    this.cardNumber = element(by.id("card-number"));
-    this.cvvNumber = element(by.id("cvv-code"));
-    this.month = element(by.xpath(".//*[@placeholder='Month']"));
-    this.year = element(by.xpath(".//*[@placeholder='Year']"));
+    this.cardtypeLI = element.all(by.css('dropdown-viamericas#card-type-select-payment-details ul.dropdown-viam-list li'));
+    this.cardtypetype = element(by.css('dropdown-viamericas#card-type-select-payment-details'));
 
-    //continue button
-    this.createButton = element(by.id("create-card-account"));
+    this.CardName = element(by.id("cardholder-name-payment-details"));
+    this.cardNickName = element(by.id("card-nickname-payment-details"));
+    this.cardNumber = element(by.id("card-number-payment-details"));
+    this.cvvNumber = element(by.id("cvv-code-payment-details"));
+
+    this.monthLI = element.all(by.css('dropdown-viamericas#expiration-month-select-payment-details ul.dropdown-viam-list li'));
+    this.month = element(by.css('dropdown-viamericas#expiration-month-select-payment-details'));
+
+    this.yearLI = element.all(by.css('dropdown-viamericas#expiration-year-select-payment-details ul.dropdown-viam-list li'));
+    this.year = element(by.css('dropdown-viamericas#expiration-year-select-payment-details'));
+
+    //Buttons
+    this.createButton = element(by.id("create-card-account-payment-details"));
+    this.backButton = element(by.id("card-go-back-payment-details"));
 
 };
 
